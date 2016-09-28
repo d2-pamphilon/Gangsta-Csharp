f535a3436940451337ec0280f998f63a2b30e981
nickstu14 <nickstu14@hotmail.co.uk>
2016-09-28 17:50:37 +0100

    Score
    Added in the Score code

---
+++ GangstaC#Jam/Assets/Scripts/Score/Score.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+
+	public Text scoreTxt;
+	public Text highScoreTxt;
+	private int score;
+	private int highScore;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+
+		highScore = 0;
+		UpdateHighScore ();
+	}
+	void Update()
+	{
+		if (score >= highScore) 
+		{
+			highScore = score;	
+			UpdateHighScore ();
+		}
+	}
+
+	public void Addscore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	// Update is called once per frame
+	void UpdateScore () {
+		scoreTxt.text = "Score: " + score;
+	}
+	void UpdateHighScore()
+	{
+		highScoreTxt.text = "High Score: " + highScore;
+	}
+}
