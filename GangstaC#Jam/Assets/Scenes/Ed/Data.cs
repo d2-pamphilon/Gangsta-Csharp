82ac58591b2a8cebd300184590a022475ec3d5d3
Ed-McD <edard_666@hotmail.co.uk>
2016-09-28 16:15:22 +0100

    Basic Scene loader + highscore saving(possibly)
    ^

---
+++ GangstaC#Jam/Assets/Scenes/Ed/Data.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Data : MonoBehaviour {
+
+    public static Data instance { get; private set; }
+
+    private string str = "HighScore";
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+	}
+
+	 public bool SetHighScore(float hs)
+    {
+        if (PlayerPrefs.HasKey(str))
+        {
+            if ( PlayerPrefs.GetFloat(str)< hs)
+            {
+                PlayerPrefs.SetFloat(str, hs);
+                PlayerPrefs.Save();
+                return true;
+            } 
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(str, hs);
+            PlayerPrefs.Save();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ GangstaC#Jam/Assets/Scenes/Ed/SceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+	public void LoadScene(string scene)
+    {
+       LoadScene(scene);
+    }
+    public void LoadScene(int scene)
+    {
+        LoadScene(scene);
+    }
+}
