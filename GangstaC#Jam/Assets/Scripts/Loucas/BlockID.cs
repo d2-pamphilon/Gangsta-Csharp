44c47253b25d9a0696bcbb03bdccbb1885966ce1
Louca Coles <coleslouca@yahoo.co.uk>
2016-09-30 14:47:36 +0100

    New Block Sprites for Multiplayer
    Added new sprites for multiplayer and added code to the blockID script
    that can be used to change it.

--- GangstaC#Jam/Assets/Scripts/Loucas/BlockID.cs
+++ GangstaC#Jam/Assets/Scripts/Loucas/BlockID.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class BlockID : MonoBehaviour {
 
 	public int blockID;
+	public Sprite blockSprite;
+	private bool spriteSet;
 
 	// Use this for initialization
 	void Start () {
-	
+		spriteSet = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!spriteSet) {
+			SetSprite(blockSprite);
+		}
 	}
 
 	public void SetBlockID(int newID) {
@@ -21,5 +25,9 @@ public class BlockID : MonoBehaviour {
 
 	public int GetBlockID() {
 		return blockID;
+	}
+
+	public void SetSprite(Sprite newSprite) {
+		gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
 	}
 }
