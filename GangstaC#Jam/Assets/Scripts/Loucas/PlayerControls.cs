24a8d7c56fc284c7670c8100fc7f9d95a7ca0a71
Louca Coles <coleslouca@yahoo.co.uk>
2016-09-30 13:07:00 +0100

    Added multiplayer controls
    Added seperate controls for multiplayer

--- GangstaC#Jam/Assets/Scripts/Loucas/PlayerControls.cs
+++ GangstaC#Jam/Assets/Scripts/Loucas/PlayerControls.cs
@@ -28,23 +28,84 @@ public class PlayerControls : MonoBehaviour {
 			//currentShape = GameObject.FindGameObjectWithTag("CurrentShape");
 		}
 		if (currentShape != null) {
-			if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-				print("Pressed Left Arrow Button");
-				currentShape.transform.position += new Vector3(-moveDistance, 0.0f, 0.0f);
+			if (playerID == 0) {
+				if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+					print("Pressed Left Arrow Button");
+					currentShape.transform.position += new Vector3(-moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKeyDown(KeyCode.RightArrow)) {
+					print("Pressed Right Arrow Button");
+					currentShape.transform.position += new Vector3(moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKey(KeyCode.DownArrow)) {
+					print("Pressed Down Arrow Button");
+					currentShape.transform.position += new Vector3(0.0f, -0.1f, 0.0f) * (Time.deltaTime * moveSpeed);
+				}
+				if (Input.GetKeyDown(KeyCode.UpArrow)) {
+					print("Pressed Down Arrow Button");
+					currentShape.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRot);
+					currentRot += 90.0f;
+				}
 			}
-			if (Input.GetKeyDown(KeyCode.RightArrow)) {
-				print("Pressed Right Arrow Button");
-				currentShape.transform.position += new Vector3(moveDistance, 0.0f, 0.0f);
+			if (playerID == 1) {
+				if (Input.GetKeyDown(KeyCode.A)) {
+					print("Pressed A Button");
+					currentShape.transform.position += new Vector3(-moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKeyDown(KeyCode.D)) {
+					print("Pressed D Button");
+					currentShape.transform.position += new Vector3(moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKey(KeyCode.S)) {
+					print("Pressed S Button");
+					currentShape.transform.position += new Vector3(0.0f, -0.1f, 0.0f) * (Time.deltaTime * moveSpeed);
+				}
+				if (Input.GetKeyDown(KeyCode.W)) {
+					print("Pressed W Button");
+					currentShape.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRot);
+					currentRot += 90.0f;
+				}
 			}
-			if (Input.GetKey(KeyCode.DownArrow)) {
-				print("Pressed Down Arrow Button");
-				currentShape.transform.position += new Vector3(0.0f, -0.1f, 0.0f) * (Time.deltaTime * moveSpeed);
+			if (playerID == 2) {
+				if (Input.GetKeyDown(KeyCode.J)) {
+					print("Pressed J Button");
+					currentShape.transform.position += new Vector3(-moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKeyDown(KeyCode.L)) {
+					print("Pressed L Button");
+					currentShape.transform.position += new Vector3(moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKey(KeyCode.K)) {
+					print("Pressed K Button");
+					currentShape.transform.position += new Vector3(0.0f, -0.1f, 0.0f) * (Time.deltaTime * moveSpeed);
+				}
+				if (Input.GetKeyDown(KeyCode.I)) {
+					print("Pressed I Button");
+					currentShape.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRot);
+					currentRot += 90.0f;
+				}
 			}
-			if (Input.GetKeyDown(KeyCode.UpArrow)) {
-				print("Pressed Down Arrow Button");
-				currentShape.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRot);
-				currentRot += 90.0f;
+			if (playerID == 3) {
+				if (Input.GetKeyDown(KeyCode.Keypad4)) {
+					print("Pressed Keypad 4 Button");
+					currentShape.transform.position += new Vector3(-moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKeyDown(KeyCode.Keypad6)) {
+					print("Pressed Keypad 6 Button");
+					currentShape.transform.position += new Vector3(moveDistance, 0.0f, 0.0f);
+				}
+				if (Input.GetKey(KeyCode.Keypad5)) {
+					print("Pressed Keypad 5 Button");
+					currentShape.transform.position += new Vector3(0.0f, -0.1f, 0.0f) * (Time.deltaTime * moveSpeed);
+				}
+				if (Input.GetKeyDown(KeyCode.Keypad8)) {
+					print("Pressed Keypad 8 Button");
+					currentShape.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRot);
+					//currentShape.transform.RotateAround
+					currentRot += 90.0f;
+				}
 			}
+
 		}
 	}
 }
