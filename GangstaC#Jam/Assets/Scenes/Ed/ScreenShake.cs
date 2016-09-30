bc246e754efde6a94cfc107f539e6ce1d9a1e2f7
Ed-McD <edard_666@hotmail.co.uk>
2016-09-30 14:44:26 +0100

    Minor improvements

--- GangstaC#Jam/Assets/Scenes/Ed/ScreenShake.cs
+++ GangstaC#Jam/Assets/Scenes/Ed/ScreenShake.cs
@@ -4,22 +4,27 @@ using System.Collections;
 public class ScreenShake : MonoBehaviour {
 
     public static ScreenShake instance { get; private set; }
-    [SerializeField]
-    private  bool testShake;
+    
+    
     private Transform thisTrans;
-    private Vector3 startPos;
-    private Vector3 newPos;    
+    private Vector3 startPos;     
     private float min = -1.0f, max = 1.0f;
     [SerializeField]
     private float shakeDuration = 0.5f;
     [SerializeField]
     private float magnitude = 5.0f;
     [SerializeField]
-    private float speed = 1.0f;
-    
+    private float speed = 2.0f;
 
-	// Use this for initialization
-	void Awake ()
+    //Debug variables
+    [SerializeField]
+    private bool testShake;
+    private bool debugCurves = false;
+    private AnimationCurve testX = new AnimationCurve();
+    private AnimationCurve testY = new AnimationCurve();
+
+    // Use this for initialization
+    void Awake ()
     {
         instance = this;
         thisTrans = transform;
@@ -27,18 +32,27 @@ public class ScreenShake : MonoBehaviour {
 	}
 
     IEnumerator Shaker()
-    {
+    {        
+        testX = debugCurves ? new AnimationCurve() : testX;
+        testY = debugCurves ? new AnimationCurve() : testY;
         float timeElapsed = 0.0f;
-        float randomStart = Random.Range(min, max);
-        while (timeElapsed < shakeDuration )
+        float randomX = Random.Range(min, max);
+        float randomY = Random.Range(min, max);
+        while (timeElapsed < shakeDuration)
         {
             timeElapsed += Time.deltaTime;
             float percent = timeElapsed / shakeDuration;
             float damping = 1.0f - Mathf.Clamp(2.0f * percent - 1.0f, 0.0f, 1.0f);
-            float alpha = randomStart + speed * percent;
+            float alphaX = randomX + speed * percent;
+            float alphaY = randomY + speed * percent;
 
-            float x = Mathf.PerlinNoise(alpha, 0) * 2.0f - 1.0f;
-            float y = Mathf.PerlinNoise(0, alpha) * 2.0f - 1.0f;
+            float x = Mathf.PerlinNoise(alphaX, 0) * 2.0f - 1.0f;
+            float y = Mathf.PerlinNoise(0, alphaY) * 2.0f - 1.0f;
+            if (debugCurves)
+            {
+                testX.AddKey(timeElapsed, x);
+                testY.AddKey(timeElapsed, y);
+            }
             x *= magnitude * damping;
             y *= magnitude * damping;
             thisTrans.position = new Vector3(x, y, thisTrans.position.z);
@@ -48,7 +62,7 @@ public class ScreenShake : MonoBehaviour {
         thisTrans.position = startPos;
     }
 
-    void Shake()
+    public void Shake()
     {
         StopCoroutine("Shaker");
         StartCoroutine("Shaker");
