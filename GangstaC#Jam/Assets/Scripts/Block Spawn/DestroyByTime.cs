e9f3d7ed7e1f665e4bad8c698900aba96ac001d0
creatayeats <April2.Yeats@live.uwe.ac.uk>
2016-10-03 13:27:29 +0100

    More spawner scripting
    Nearly complete spawner script, previous bugs now ironed out
    to do: isKinematic false on current spawner
    list of blocks, and read blocks from list

---
+++ GangstaC#Jam/Assets/Scripts/Block Spawn/DestroyByTime.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float lifetime = 10f;
+    public Rigidbody rb;
+    private bool collided;
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, lifetime);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SpawnerCurrBlock")
+        {
+            collided = true;
+        }
+
+        //Turn on kinematic when block hits the floor, or other blocks
+        //if (other.tag == "Floor")
+        //{
+        //    collided = false;
+        //}
+    }
+
+    void Update ()
+    {
+        if (collided)
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
+    }
+    
+
+	
+}
--- GangstaC#Jam/Assets/Scripts/Block Spawn/Spawner.cs
+++ GangstaC#Jam/Assets/Scripts/Block Spawn/Spawner.cs
@@ -4,31 +4,26 @@ using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour {
 
-    List<int> blockQ = new List<int>();
     public GameObject[] blockPrefab;
-    public Transform[] spawnPoints;         // Spawn from either current or next spawner
+    public Transform[] spawnPoints;
+    public int blockCount = 64;             // Amount of usable blocks
     public float spawnTime = 3f;            // How long between each spawn
-
 
     // Use this for initialization
     void Start () {
 
-        for (int i = 0; i < 10; i++)
-        {
-            blockQ.Add(Random.Range(0, 6));
-        }
-        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-
+        StartCoroutine(Spawn());
     }
 
-    void Spawn()
+    IEnumerator Spawn()
     {
-        for (int j = 0; j < 10; j++)
+        for (int i = 0; i < blockCount; i++)
         {
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-            Instantiate(blockPrefab[blockQ[j]], spawnPoints[0].position, spawnPoints[0].rotation);
-            Instantiate(blockPrefab[blockQ[j]+1], spawnPoints[1].position, spawnPoints[0].rotation);
+            Instantiate(blockPrefab[Random.Range(0, 6)], spawnPoints[0].position, spawnPoints[0].rotation);
+            Instantiate(blockPrefab[Random.Range(0, 6)], spawnPoints[1].position, spawnPoints[0].rotation);
+            yield return new WaitForSeconds(spawnTime);
         }
+ 
     }
 }
