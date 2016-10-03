5f5dff1907186b3fb69824af78683084d58a6bd1
Ed-McD <edard_666@hotmail.co.uk>
2016-10-03 14:01:22 +0100

    Main Menu scene loads Game Scene

---
+++ GangstaC#Jam/Assets/Scenes/Ed/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    private bool loading = false;
+
+    public void testPrint()
+    {
+        Debug.Log("Button Pressed");
+    }
+
+    public void LoadNewScene(string scene)
+    {
+        if (!loading)
+        {
+            loading = true;
+            SceneManager.LoadScene(scene);
+        }
+    }
+    public void LoadNewScene(int scene)
+    {
+        if (!loading)
+        {
+            loading = true;
+            SceneManager.LoadScene(scene);
+        }
+
+    }
+}
