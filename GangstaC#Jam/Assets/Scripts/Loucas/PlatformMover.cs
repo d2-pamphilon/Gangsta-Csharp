a291bb05df5b0bba53279db389230ec3ed32d174
Louca Coles <coleslouca@yahoo.co.uk>
2016-10-03 14:40:33 +0100

    Simple Platformer Spawner
    Added a simple platformer spawner

---
+++ GangstaC#Jam/Assets/Scripts/Loucas/PlatformMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMover : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0) * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GangstaC#Jam/Assets/Scripts/Loucas/PlatformSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	public GameObject[] platformPrefabs;
+	public float spawnWait, startWait, newForce, speed;
+	public int platformCount;
+	public Vector3 spawnValues;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnPlatform());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnPlatform() {
+		yield return new WaitForSeconds(startWait);
+		while(true) {
+			platformCount = Random.Range(1, 3);
+			for (int i = 0; i < platformCount; i++) {
+				Vector3 spawnPos = new Vector3(spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y), 0.0f);
+				Quaternion spawnRot = Quaternion.identity;
+				GameObject tempPlatform = (GameObject)Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)], spawnPos, spawnRot);
+				tempPlatform.GetComponent<Rigidbody2D>().AddForce(new Vector2(newForce, 0) * (Time.deltaTime * speed));
+				yield return new WaitForSeconds (spawnWait);
+			}
+		}
+	}
+}
