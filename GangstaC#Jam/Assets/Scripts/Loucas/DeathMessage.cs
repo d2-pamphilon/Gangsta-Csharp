a403f5123017ce570ce17c5eab1c103a85274786
d2-pamphilon <Daniel2.Pamphilon@live.uwe.ac.uk>
2016-10-04 17:26:21 +0100

    restart lvl on death

--- GangstaC#Jam/Assets/Scripts/Loucas/DeathMessage.cs
+++ GangstaC#Jam/Assets/Scripts/Loucas/DeathMessage.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class DeathMessage : MonoBehaviour {
 
 	public GameObject runner;
-	public Text deathText;
+	public Text deathText, promptText;
 	public string[] deathMessages;
 	bool spawnedMessage;
 	Color[] colors  = {Color.red, Color.cyan, Color.yellow , Color.green, Color.magenta, Color.white};
 
 	// Use this for initialization
 	void Start () {
+        Time.timeScale = 1;
 		deathText.text = "";
+        promptText.text = "";
 		spawnedMessage = false;
 	}
 	
@@ -20,8 +23,21 @@ public class DeathMessage : MonoBehaviour {
 	void Update () {
 		if (runner.GetComponent<AIJumpScript>().m_dead && !spawnedMessage) {
 			deathText.text = deathMessages[(int)Random.Range(0, deathMessages.Length)];
-			deathText.color = colors[(int)Random.Range(0, colors.Length)];
+            promptText.text = "Press ESC to restart";
+            deathText.color = colors[(int)Random.Range(0, colors.Length)];
+            promptText.color = deathText.color;
 			spawnedMessage = true;
 		}
-	}
+        if (spawnedMessage)
+        {
+            Time.timeScale = 0;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                //print("exit game");
+                SceneManager.LoadScene(1);
+                //  Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
 }
