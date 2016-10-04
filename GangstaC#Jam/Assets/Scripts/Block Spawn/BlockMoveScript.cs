c024451b7b5d1355a1423ab66d7f2ce38704d558
creatayeats <createayeats@googlemail.com>
2016-10-04 23:06:04 +0100

    Playability and Audio commit
    Added some background music for the game, and attempted to make the game
    a bit more playable by allowing the blocks to hit the floor and 'build
    up', slowing down scroll speed, raising jump speed, prolonging time
    until first platfrom vanishes

---
+++ GangstaC#Jam/Assets/Scripts/Block Spawn/BlockMoveScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMoveScript : MonoBehaviour {
+
+    [SerializeField] private bool hasInitiallyCollided;
+    [SerializeField] private bool placed;
+    [SerializeField] private bool alive;
+
+    // Use this for initialization
+    void Start () {
+        alive = true;
+        placed = false;
+        hasInitiallyCollided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!hasInitiallyCollided)
+        {
+            PlatformMoverScript mover = gameObject.AddComponent<PlatformMoverScript>();
+            mover.speed = 5.0f;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            gameObject.tag = "Terrain";
+            hasInitiallyCollided = true;
+            placed = true;
+            ScreenShake.instance.Shake();
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bottom")
+        {
+            PlatformMoverScript mover = gameObject.AddComponent<PlatformMoverScript>();
+            mover.speed = 5.0f;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            hasInitiallyCollided = true;
+            placed = true;
+        }
+    }
+
+    public bool IsAlive()
+    {
+        return alive;
+    }
+
+    public bool IsPlaced()
+    {
+        return placed;
+    }
+}
