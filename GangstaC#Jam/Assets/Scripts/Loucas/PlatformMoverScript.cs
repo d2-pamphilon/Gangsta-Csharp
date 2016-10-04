6ce405439fc3f7324176376369b5bed2d245ae3a
AnthonyYoManz <anthony2.arian@live.uwe.ac.uk>
2016-10-04 15:02:33 +0100

    Aaaand the lsat one (hopefully)

---
+++ GangstaC#Jam/Assets/Scripts/Loucas/PlatformMoverScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMoverScript : MonoBehaviour {
+
+	public float speed;
+	//private Vector3 targetPos;
+
+
+	// Use this for initialization
+	void Start () {
+		//targetPos = new Vector3(-15.0f, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 vel = new Vector2(-speed, 0);
+        //transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        Rigidbody2D physics = GetComponent<Rigidbody2D>();
+        if(physics != null)
+        { 
+            physics.velocity = vel;
+        }
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Kill") {
+			Destroy(gameObject);
+		}
+	}
+}
